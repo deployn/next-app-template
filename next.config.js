094a2db98629fd511a596d7c3e7029e8b0c1daa9
Jewgeni Lewash <j.lewash@gmail.com>
2023-10-05 07:33:15 +0200

    feat: remove mdx support

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   output: 'export',
   distDir: 'out',
-  experimental: {
-    mdxRs: true,
-  },
 }
 
-const withMDX = require('@next/mdx')()
-const { withContentlayer } = require('next-contentlayer')
-module.exports = withMDX(withContentlayer(nextConfig))
+module.exports = nextConfig
